ca921e29d8e29c9f38f218e21eeadd9b977600ef
casperenghuus <casper.enghuus@users.noreply.github.com>
2014-11-11 18:35:29 -0500

    RNA_bin_merge.py

--- RNA_bin_merge.py
+++ RNA_bin_merge.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
         all_results = os.path.join(CWD, 'sequences.allresults.rna.txt')
         all_stats = os.path.join(CWD, 'stats.allresults.rna.txt')
 
-    # RNA_contigs.seq_counts()
-    # RNA_contigs.merge_bins()
+    RNA_contigs.seq_counts()
+    RNA_contigs.merge_bins()
     RNA_contigs.run_bowtie()
     RNA_contigs.bowtie_to_file(all_results)
     RNA_contigs.final_stats(all_stats, all_results)
