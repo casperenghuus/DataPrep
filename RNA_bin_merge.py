cd3972c9349761c428ec7ec2cd99e8dfeb53566b
casperenghuus <casper.enghuus@users.noreply.github.com>
2014-11-25 14:40:52 -0500

    RNA_bin_merge.py

--- RNA_bin_merge.py
+++ RNA_bin_merge.py
@@ -19,10 +19,21 @@ if __name__ == "__main__":
     if ftype == 'RNA':
         all_results = os.path.join(CWD, 'sequences.allresults.rna.txt')
         all_stats = os.path.join(CWD, 'stats.allresults.rna.txt')
+    if ftype == 'Small':
+        all_results = os.path.join(CWD, 'sequences.allresults.small.rna.txt')
+        all_stats = os.path.join(CWD, 'stats.allresults.small.rna.txt')
 
-    RNA_contigs.seq_counts()
-    RNA_contigs.merge_bins(ftype)
-    RNA_contigs.run_bowtie()
-    RNA_contigs.bowtie_to_file(all_results)
-    RNA_contigs.final_stats(all_stats, all_results)
+    if ftype == 'Small':
+        RNA_small_contigs.seq_counts()
+        RNA_small_contigs.merge_bins(ftype)
+        RNA_small_contigs.run_bowtie()
+        RNA_small_contigs.bowtie_to_file(all_results)
+        RNA_small_contigs.final_stats(all_stats, all_results)
+
+    if ftype != 'Small':
+        # RNA_contigs.seq_counts()
+        RNA_contigs.merge_bins(ftype)
+        RNA_contigs.run_bowtie()
+        RNA_contigs.bowtie_to_file(all_results)
+        RNA_contigs.final_stats(all_stats, all_results)
 
